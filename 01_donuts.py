169db200478ff29a9f32d813d2d2d302242238db
Eduardo Alencar <ealencar10@gmail.com>
2020-10-15 19:16:42 -0300

    implementada funcionalidade para numero de donuts inferior a 10

--- 01_donuts.py
+++ 01_donuts.py
@@ -11,7 +11,7 @@ e donuts(23) retorna 'Number of donuts: many'
 
 def donuts(count):
     # +++ SUA SOLUÇÃO +++
-    return
+    return f'Number of donuts: {count}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
